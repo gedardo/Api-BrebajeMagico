ebe4e738adf8e7f309903f96f1f2ef67793a46a4
Gera Navarro <gerardonavarro19@gmail.com>
2022-12-01 09:24:00 -0300

    agregado cartContest

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React from "react";
 import "./css/App.css";
 import { ItemListContainer } from "./components/ItemListContainer";
 import { Layout } from "./components/Layout";
+import { CartContextProvider } from './context/cartContext';
+
 
 function App() {
   return (
-    <Layout>
-      <ItemListContainer />
-    </Layout>
+    <CartContextProvider>
+      <Layout>
+        <ItemListContainer />
+      </Layout>
+    </CartContextProvider>
   );
 }
 
---
+++ src/context/cartContext.js
@@ -0,0 +1,29 @@
+import React, { createContext, useState } from 'react';
+
+export const Context = createContext();
+
+export const CartContextProvider = ({ children }) => {
+  const [carrito, setCarrito] = useState([]);
+
+  function addItem(item) {
+    setCarrito(carrito.push(item));
+  }
+
+  function removeItem(itemId) {
+    setCarrito(carrito.filter((item) => item.id !== itemId));
+  }
+
+  function clear() {
+    setCarrito([]);
+  }
+
+  function isInCart(itemId) {
+    return Boolean(carrito.find((item) => item.id === itemId));
+  }
+
+  return (
+    <Context.Provider value={{ addItem, removeItem, clear, isInCart }}>
+      {children}
+    </Context.Provider>
+  );
+};
